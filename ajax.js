eca72c6cd26815655b73ad08f91461f4751c51df
Mazarul Islam <mazarulislam@Mazaruls-MBP.mshome.net>
2018-04-07 02:22:45 -0400

    final touches

--- ajax.js
+++ ajax.js
@@ -85,48 +85,97 @@ to output the info.. it would have been glorious!!!!!
 
 //boring axios call
 
-let ul = document.querySelector("ul")
+let ul = document.querySelector("ul");
 class fullMetal {
-	constructor() {
-		this.all = []
-	}
-	add(pokemon) {
-		this.all.push(pokemon)
-	}
+  constructor() {
+    this.all = [];
+  }
+  add(pokemon) {
+    this.all.push(pokemon);
+  }
 }
 
 class Pokemon {
-	constructor(name, hp, attack, defense, abilities) {
-		this.name = name;
-		this.hp = hp;
-		this.attack = attack;
-		this.defense = defense;
-		this.abilities = abilities;
-	}
+  constructor(name, hp, attack, defense, abilities) {
+    this.name = name;
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+    this.abilities = abilities;
+  }
 }
 
+axios
+  .get("https://pokeapi.co/api/v2/pokemon/791/")
+  .then(response => {
+    let data = response.data;
+    let name = document.querySelector("#solname");
+    let hp = document.querySelector("#solhp");
+    let attack = document.querySelector("#solattack");
+    let defence = document.querySelector("#soldefence");
+    let ability1 = document.querySelector("#solability1");
+    let ability2 = document.querySelector("#solability2");
+    let ability3 = document.querySelector("#solability3");
 
-axios.get("https://pokeapi.co/api/v2/pokemon/791/")
-	.then((response) => {
-		let data = response.data
-        let name = document.querySelector("#solname")
-        let hp = document.querySelector("#solhp")
-        let attack = document.querySelector("#solattack")
-        let defence = document.querySelector("#soldefence")
-		let ability1 = document.querySelector("#solability1")
-		let ability2 = document.querySelector("#solability2")
-		let ability3 = document.querySelector("#solability3")
+    let solgaleo = new Pokemon();
+    name.innerHTML = data.name;
+    hp.innerHTML = "Hp: " + data.stats[5].base_stat;
+    attack.innerHTML = "Attk: " + data.stats[4].base_stat;
+    defence.innerHTML = "Def: " + data.stats[3].base_stat;
+    ability1.innerHTML = data.abilities[0].ability.name;
+    // ability2.innerHTML = data.abilities[1].ability.name;
+    // ability3.innerHTML = data.abilities[2].ability.name;
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-		let solgaleo = new Pokemon()
-		name.innerHTML = data.name
-		hp.innerHTML = "Hp: " + data.stats[5].base_stat
-		attack.innerHTML = "Attk: " + data.stats[4].base_stat
-		defence.innerHTML = "Def: " + data.stats[3].base_stat
-		ability1.innerHTML = data.abilities[0].ability.name
-		ability2.innerHTML = data.abilities[1].ability.name
-		ability3.innerHTML = data.abilities[2].ability.name
+  axios
+  .get("https://pokeapi.co/api/v2/pokemon/405/")
+  .then(response => {
+    let data = response.data;
+    let name = document.querySelector("#luxname");
+    let hp = document.querySelector("#luxhp");
+    let attack = document.querySelector("#luxattack");
+    let defence = document.querySelector("#luxdefence");
+    let ability1 = document.querySelector("#luxability1");
+    let ability2 = document.querySelector("#luxability2");
+    let ability3 = document.querySelector("#luxability3");
 
-	})
-	.catch((error) => {
-		console.log(error)
-	})
+    let luxray = new Pokemon();
+    // name.innerHTML = data.name;
+    hp.innerHTML = "Hp: " + data.stats[5].base_stat;
+    attack.innerHTML = "Attk: " + data.stats[4].base_stat;
+    defence.innerHTML = "Def: " + data.stats[3].base_stat;
+    ability1.innerHTML = data.abilities[0].ability.name;
+    // ability2.innerHTML = data.abilities[1].ability.name;
+    // ability3.innerHTML = data.abilities[2].ability.name;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+  axios
+  .get("https://pokeapi.co/api/v2/pokemon/658/")
+  .then(response => {
+    let data = response.data;
+    // let name = document.querySelector("#gname");
+    let hp = document.querySelector("#ghp");
+    let attack = document.querySelector("#gattack");
+    let defence = document.querySelector("#gdefence");
+    let ability1 = document.querySelector("#gability1");
+    let ability2 = document.querySelector("#gability2");
+    let ability3 = document.querySelector("#gability3");
+
+    let greninja = new Pokemon();
+    name.innerHTML = data.name;
+    hp.innerHTML = "Hp: " + data.stats[5].base_stat;
+    attack.innerHTML = "Attk: " + data.stats[4].base_stat;
+    defence.innerHTML = "Def: " + data.stats[3].base_stat;
+    ability1.innerHTML = data.abilities[0].ability.name;
+    // ability2.innerHTML = data.abilities[1].ability.name;
+    // ability3.innerHTML = data.abilities[2].ability.name;
+  })
+  .catch(error => {
+    console.log(error);
+  });
